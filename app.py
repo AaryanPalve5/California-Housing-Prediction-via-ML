148df3b41697a985642fb36a26b7ff81ba3ed050
AaryanPalve5 <aspalve2004@gmail.com>
2024-05-09 20:11:17 +0530

    modified app.py

--- app.py
+++ app.py
@@ -19,36 +19,36 @@ except FileNotFoundError:
 # Function to preprocess input data and make prediction
 def predict_price(data):
     # Preprocess input data using the scaler
-    input_data = np.array([[data['MedInc'], data['HouseAge'], data['AveRooms'], data['AveBedrms'],
+    input_data = np.array([[data['Median_Income'], data['HouseAge'], data['AveRooms'], data['AveBedrms'],
                             data['Population'], data['AveOccup'], data['Latitude'], data['Longitude']]])
     scaled_input_data = scaler.transform(input_data)
 
     # Make prediction using the regression model
     prediction = model.predict(scaled_input_data)[0]
-    return prediction*100000 
+    return prediction * 100000 
 
 # Streamlit UI
 def main():
     st.title('California Housing Price Prediction')
-
+    
     # Input form
     st.header('Input Data')
-    med_inc = st.slider('MedInc', min_value=1, max_value=15, step=1)
-    house_age = st.slider('HouseAge', min_value=1, max_value=50, step=1)
-    ave_rooms = st.slider('AveRooms', min_value=1, max_value=10, step=1)
-    ave_bedrms = st.slider('AveBedrms', min_value=1, max_value=5, step=1)
+    median_income = st.slider('Median Income', min_value=1, max_value=15, step=1)
+    house_age = st.slider('House Age', min_value=1, max_value=50, step=1)
+    ave_rooms = st.slider('Average Rooms', min_value=1, max_value=10, step=1)
+    ave_bedrooms = st.slider('Average Bedrooms', min_value=1, max_value=5, step=1)
     population = st.slider('Population', min_value=1, max_value=5000, step=10)
-    ave_occup = st.slider('AveOccup', min_value=1, max_value=10, step=1)
+    ave_occupancy = st.slider('Average Occupancy', min_value=1, max_value=10, step=1)
     latitude = st.slider('Latitude', min_value=32, max_value=42, step=1)
     longitude = st.slider('Longitude', min_value=-125, max_value=-114, step=1)
 
     input_data = {
-        'MedInc': med_inc,
+        'Median_Income': median_income,
         'HouseAge': house_age,
         'AveRooms': ave_rooms,
-        'AveBedrms': ave_bedrms,
+        'AveBedrms': ave_bedrooms,
         'Population': population,
-        'AveOccup': ave_occup,
+        'AveOccup': ave_occupancy,
         'Latitude': latitude,
         'Longitude': longitude
     }
